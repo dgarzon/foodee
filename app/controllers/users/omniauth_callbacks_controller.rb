8ed81b18c28fc4b8007d1b498b38c713da9fca8f
pranavbhalla89 <Apple@Apples-MacBook-Pro.local>
2014-03-13 02:41:45 -0400

    graph api call

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if user.valid?
       user.sign_in_count <= 1 ? flash[:success] = "Welcome to Foodee, #{user.first_name}!" : flash[:notice] = "Welcome back, #{user.first_name}!"
+      # need to get updated friends list here and save in current user session
+      @graph = Koala::Facebook::API.new(identity.token)
+      graphFriends = @graph.get_connections("me", "friends")
+      # friendsArray = JSON.parse graphFriends
+      # logger.debug "friends : #{friendsArray.inspect}"
+      sessionFriends = []
+      graphFriends.each do |friend|
+        sessionFriends << friend["id"]
+      end
+      session[:friends] = sessionFriends
+
+      # redirect to looged in page
       sign_in_and_redirect user
     else
       sign_in user
