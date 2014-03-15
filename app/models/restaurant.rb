11374bbcb9c1e2db3c15383e04136fea473df2ed
Daniel Garzon <dg2796@columbia.edu>
2014-03-14 22:57:46 -0400

    Fixed something that was fixed before, but ...

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,13 +10,10 @@ class Restaurant < ActiveRecord::Base
 
 	def self.get_restaurant_by_address (address, term)
 		client = Yelp::Client.new(:debug => true)
-	  # request = GeoPoint.new(
-			# 				             :term => "food",
-			# 				             :latitude => address.latitude,
-			# 				             :longitude => address.longitude)
-	  request = Location.new(
-			:address => term,
-			:term => "food")
+	  request = GeoPoint.new(
+							             :term => term,
+							             :latitude => address.latitude,
+							             :longitude => address.longitude)
 		response = client.search(request)
 		response
 	end
